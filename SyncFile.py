f76d24615f04b7fa8ddd47c29af0f98ae9823976
Lanceshi2 <Lanceyshi2@gmail.com>
2014-11-05 20:53:05 +1000

    Using list for the settings file

--- SyncFile.py
+++ SyncFile.py
@@ -1,46 +1,51 @@
-
+import shutil
 import sublime
 import sublime_plugin
-import shutil 
 
 class SyncFileCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         settings = sublime.load_settings('SyncFile.sublime-settings')
-        source1  = settings.get('sf_source_1')
-        dest1    = settings.get('sf_dest_1')
-        source2  = settings.get('sf_source_2')
-        dest2    = settings.get('sf_dest_2')
+        src_mappings = settings.get("mappings", [])
+        mappings = []
 
-        source3  = settings.get('sf_source_3')
-        dest3    = settings.get('sf_dest_3')
+        # Check if we have the correct value type
+        if not isinstance(src_mappings, list):
+            print("invalid value type, should be a list: %r" % src_mappings)
+            return
+        for mapping in src_mappings:
+            
+            # Check if we have the correct value types for the mappings
+            if not isinstance(mapping, dict):
+                print("invalid mapping type, should be a dict: %r" % mapping)
+                continue
+            # Check if required keys exist
+            elif not all(name in mapping for name in ('source', 'dest')):
+                print("required key(s) missing: %r" % mapping)
+                continue
+            # Check if required keys have correct value type
+            elif not isinstance(mapping['source'], str) or not isinstance(mapping['dest'], str):
+                print("invalid type for required key(s), should be str: %r" % mapping)
+                continue
+            # Check if required keys are not empty
+            elif not mapping['source'] or not mapping['dest']:
+                print("required key(s) empty: %r" % mapping)
+                continue
+            else:
+                mappings.append(mapping)
 
-        source4  = settings.get('sf_source_4')
-        dest4    = settings.get('sf_dest_4')
+        # Check if there are valid mappings
+        if not mappings:
+            print("No valid mappings found")
+            return
 
-        source5  = settings.get('sf_source_5')
-        dest5    = settings.get('sf_dest_5')
+        source_name = self.view.file_name()
 
-        sourceFileName = self.view.file_name()
-        destFileName   = sourceFileName
-        
-        if source1 != None and source1 != "" and dest1 != None and dest1 != "" and source1 in sourceFileName:
-            destFileName = sourceFileName.replace(source1, dest1)
-        elif source2 != None and source2 != "" and dest2 != None and dest2 != "" and source2 in sourceFileName:
-            destFileName = sourceFileName.replace(source2, dest2)
-        elif source3 != None and source3 != "" and dest3 != None and dest3 != "" and source3 in sourceFileName:
-            destFileName = sourceFileName.replace(source3, dest3)
-        elif source4 != None and source4 != "" and dest4 != None and dest4 != "" and source4 in sourceFileName:
-            destFileName = sourceFileName.replace(source4, dest4)
-        elif source5 != None and source5 != "" and dest5 != None and dest5 != "" and source5 in sourceFileName:
-            destFileName = sourceFileName.replace(source5, dest5)
+        for mapping in mappings:
+            if mapping['source'] in source_name and mapping['dest'] != None and mapping['dest'] != "":
+                shutil.copyfile(source_name, source_name.replace(mapping['source'], mapping['dest']))
+                return
         else:
             msg = 'Your current file location is not in one of your source locations '
             msg += 'or the relevant dest location is empty. '
             msg += 'Please set the settings file properly and retry.'
-            sublime.error_message(msg)  
-            raise BaseException
-
-        #self.view.insert(edit, 0, sourceFileName + '\n')
-        #self.view.insert(edit, 0, destFileName + '\n')
-
-        shutil.copyfile(sourceFileName, destFileName)
+            sublime.error_message(msg)
